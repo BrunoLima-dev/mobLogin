9bfb30bbcc8193616164ef7e55200926aa6f4c6a
Bruno Lima <brunoprograme04@gmail.com>
2022-12-29 10:27:05 -0300

    Crio duas telas sem formatação

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Signin from './pages/Signin';
+import { ScreenA } from './src/screens/ScreenA';
 
 export default function App() {
   return (
-    <>
-      <Signin />
-      <StatusBar style="light" />
-    </>
+    // <>
+    //   <Signin />
+    //   <StatusBar style="light" />
+    // </>
+    <ScreenA/>
+    
   );
 }
